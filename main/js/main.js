45a8a698b91ffc6cbe18e69a6c275fbc5aa30427
Alejandro Soares <soaresalejandro@outlook.com>
2021-12-07 15:27:59 -0300

    remove: old fields; fix: minify.py

---
+++ main/js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", e => {
+
+    console.log("Content loaded")
+})
+
+function add(){
+    console.log("asd")
+}
+function add(){
+    console.log("asd")
+}
+function add(){
+    console.log("asd")
+}
+function add(){
+    console.log("asd")
+}
+function add(){
+    console.log("asd")
+}
+function add(){
+    console.log("asd")
+}
+function add(){
+    console.log("asd")
+}
+function add(){
+    console.log("asd")
+}
+function add(){
+    console.log("asd")
+}
